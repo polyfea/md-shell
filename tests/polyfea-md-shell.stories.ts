93abdcb5f892e0959d0694f3769edab5253adb28
Milan Unger <milan.unger@siemens-healthineers.com>
2026-01-18 13:48:53 +0100

    -

--- tests/polyfea-md-shell.stories.ts
+++ tests/polyfea-md-shell.stories.ts
@@ -656,9 +656,9 @@ export const Drawer: Story = {
     await step("Close by clicking on drawer", async () => {
       const drawer = deepQuerySelector("drawer", shell.shadowRoot!) as HTMLElement;
       await expect(drawer).toBeInTheDocument();
+      const page = deepQuerySelector("page", shell.shadowRoot!) as HTMLElement;
+      await expect(page).toHaveAttribute("size", "medium");
       drawer.dispatchEvent(new MouseEvent("click", { bubbles: true, composed: true }));
-
-      
       await waitFor(async () => {
         await expect(drawer).not.toHaveAttribute("open");
       }, { timeout: 3000 });
