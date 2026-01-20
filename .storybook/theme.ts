93056187c59c95055bf112c2a9491e93efe6f4a9
Milan Unger <30626125+milung@users.noreply.github.com>
2026-01-20 15:05:04 +0100

    FIX: locale-menu uses undefined in call to map(locales, ...) (#14)

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -4,6 +4,6 @@ export default create({
     base: 'dark',
   brandTitle: 'Polyfea MD Shell',
   brandUrl: 'https://polyfea.github.io/md-shell/',
-  brandImage: '/logo.svg',
+  brandImage: './logo.svg',
   brandTarget: '_self',
 });
